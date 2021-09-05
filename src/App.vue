044d5552bc8664a846a991a824bcdc96e41b50cf
Hung Dinh <hunk.frank93@gmail.com>
2021-09-05 15:49:48 +0700

    init router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Navigator />
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Vue from "vue"
+import  Navigator  from "@/shared/Navigator.vue"
+export default Vue.extend({
+  name:"App",
+  components:{
+    Navigator
   }
-}
+})
 </script>
 
 <style lang="scss">
 @import "@/assets/scss/layout/main.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
 
 </style>
